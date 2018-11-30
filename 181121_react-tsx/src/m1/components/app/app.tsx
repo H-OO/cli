739da45ed00489c34e406150af79953f17571060
H-OO <38312623+H-OO@users.noreply.github.com>
2018-11-30 17:35:36 +0800

    update

--- 181121_react-tsx/src/m1/components/app/app.tsx
+++ 181121_react-tsx/src/m1/components/app/app.tsx
@@ -6,12 +6,21 @@ import * as axios from 'axios';
 class App extends React.Component {
   public constructor(arg: any) {
     super(arg);
+    this.foo = this.foo.bind(this);
+  }
+  public async foo() {
+    // 异步
+    const res = await 'res!!';
+    return res;
   }
   public componentWillMount(): void {
     console.log(
       _.join(['Another', 'module', 'loaded!'], ' ')
     );
     console.log(axios);
+    this.foo().then((res) => {
+      console.log(res);
+    });
   }
   public render(): JSX.Element {
     return (
